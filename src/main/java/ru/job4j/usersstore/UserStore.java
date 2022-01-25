cc10841a7f6ee43a556b61b9c6074d05efe428f6
NikolayKharkov <vtiko564@gmail.com>
2022-01-25 18:58:35 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #255464]

--- src/main/java/ru/job4j/usersstore/UserStore.java
+++ src/main/java/ru/job4j/usersstore/UserStore.java
@@ -9,27 +9,25 @@ public class UserStore {
     private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
 
     public boolean add(User user) {
-        return users.putIfAbsent(user.getId(), User.of(user.getId(), user.getAmount())) != null;
+        return !(users.putIfAbsent(user.getId(), user) == null);
     }
 
     public boolean delete(User user) {
-        return users.remove(user.getId()) != null;
+        return !(users.remove(user.getId()) == null);
     }
 
     public boolean update(User user) {
-        return users.computeIfPresent(user.getId(), (k, v) -> User.of(user.getId(), user.getAmount())) != null;
+        return !(users.computeIfPresent(user.getId(), (k, v) -> user) == null);
     }
 
     public boolean transfer(int fromId, int toId, int amount) {
         boolean result = false;
-        if (fromId != toId
-                && amount > 0
-                && users.containsKey(fromId)
-                && users.containsKey(toId)
-                && users.get(fromId).getAmount() >= amount) {
-            User from = User.of(users.get(fromId).getId(), users.get(fromId).getAmount() - amount);
-            User to = User.of(users.get(toId).getId(), users.get(toId).getAmount() + amount);
-            result = update(from) && update(to);
+        User from = users.get(fromId);
+        User to = users.get(toId);
+        if (!(from == null && to == null) && from.getAmount() >= amount) {
+            from.setAmount(from.getAmount() - amount);
+            to.setAmount(to.getAmount() + amount);
+            result = true;
         }
         return result;
     }
