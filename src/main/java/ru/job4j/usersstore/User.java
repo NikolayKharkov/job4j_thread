42d22567ced614c369e4cc1601300af5063d47d0
NikolayKharkov <vtiko564@gmail.com>
2022-01-26 12:54:05 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #255464]

--- src/main/java/ru/job4j/usersstore/User.java
+++ src/main/java/ru/job4j/usersstore/User.java
@@ -28,4 +28,17 @@ public class User {
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/ru/job4j/usersstore/UserStore.java
+++ src/main/java/ru/job4j/usersstore/UserStore.java
@@ -9,22 +9,22 @@ public class UserStore {
     private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
 
     public boolean add(User user) {
-        return !(users.putIfAbsent(user.getId(), user) == null);
+        return users.putIfAbsent(user.getId(), user) != null;
     }
 
     public boolean delete(User user) {
-        return !(users.remove(user.getId()) == null);
+        return users.remove(user.getId(), user);
     }
 
     public boolean update(User user) {
-        return !(users.computeIfPresent(user.getId(), (k, v) -> user) == null);
+        return users.computeIfPresent(user.getId(), (k, v) -> user) != null;
     }
 
     public boolean transfer(int fromId, int toId, int amount) {
         boolean result = false;
         User from = users.get(fromId);
         User to = users.get(toId);
-        if (!(from == null && to == null) && from.getAmount() >= amount) {
+        if (from != null && to != null && from.getAmount() >= amount) {
             from.setAmount(from.getAmount() - amount);
             to.setAmount(to.getAmount() + amount);
             result = true;
